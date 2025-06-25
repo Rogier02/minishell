1c4c9e8c38cbe5bda1ab840c69f7c7aed039633c
Mahmut Kilic <mahkilic@f1r6s8.codam.nl>
2025-06-25 16:29:46 +0200

    fixed run_builtin

--- srcs/execution/executor.c
+++ srcs/execution/executor.c
@@ -12,46 +12,27 @@
 
 #include "minishell.h"
 
-static int	is_builtin(char *cmd)
-{
-	if (!cmd)
-		return (0);
-	if (!ft_strncmp(cmd, "echo", 5))
-		return (1);
-	if (!ft_strncmp(cmd, "cd", 3))
-		return (1);
-	if (!ft_strncmp(cmd, "pwd", 4))
-		return (1);
-	if (!ft_strncmp(cmd, "export", 7))
-		return (1);
-	if (!ft_strncmp(cmd, "unset", 6))
-		return (1);
-	if (!ft_strncmp(cmd, "env", 4))
-		return (1);
-	if (!ft_strncmp(cmd, "exit", 5))
-		return (1);
-	return (0);
-}
-
 static int	run_builtin(t_minishell *minishell)
 {
 	char	**cmd;
 
 	cmd = minishell->cmd_table->cmd;
+	if (!cmd || !cmd[0])
+		return (0);
 	if (!ft_strncmp(cmd[0], "echo", 5))
-		return (ft_echo(cmd));
+		return (ft_echo(cmd), 1);
 	if (!ft_strncmp(cmd[0], "cd", 3))
-		return (ft_cd(minishell, cmd));
+		return (ft_cd(minishell, cmd), 1);
 	if (!ft_strncmp(cmd[0], "pwd", 3))
-		return (ft_pwd());
+		return (ft_pwd(), 1);
 	if (!ft_strncmp(cmd[0], "export", 7))
-		return (ft_export(minishell, cmd));
+		return (ft_export(minishell, cmd), 1);
 	if (!ft_strncmp(cmd[0], "unset", 6))
-		return (ft_unset(minishell, cmd));
+		return (ft_unset(minishell, cmd), 1);
 	if (!ft_strncmp(cmd[0], "env", 3))
-		return (ft_env(minishell, cmd));
+		return (ft_env(minishell, cmd), 1);
 	if (!ft_strncmp(cmd[0], "exit", 5))
-		return (ft_exit(minishell, cmd));
+		return (ft_exit(minishell, cmd), 1);
 	return (0);
 }
 
@@ -59,7 +40,7 @@ int	executor(t_minishell *msh)
 {
 	if (!msh->cmd_table || !msh->cmd_table->cmd || !msh->cmd_table->cmd[0])
 		return (-1);
-	if (is_builtin(msh->cmd_table->cmd[0]))
-		return (run_builtin(msh));
+	if (run_builtin(msh))
+		return (0);
 	return (exec_cmd(msh));
 }
