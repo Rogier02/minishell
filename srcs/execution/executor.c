db9acd88a6ffe1453582f997b05610b166652c32
Mahmut Kilic <mahkilic@f1r5s21.codam.nl>
2025-07-02 21:45:24 +0200

    pipes and redirections

--- srcs/execution/executor.c
+++ srcs/execution/executor.c
@@ -36,11 +36,108 @@ static int	run_builtin(t_minishell *minishell)
 	return (0);
 }
 
+// handle infile
+static void	handle_infile(t_minishell *msh, t_cmd_table *cmd)
+{
+	int infd;
+
+	infd = open(cmd->infile, O_RDONLY);
+	if (infd < 0)
+		error_and_exit(cmd->infile, msh);
+	dup2(infd, STDIN_FILENO);
+	close(infd);
+}
+
+// handle outfile
+static void	handle_outfile(t_minishell *msh, t_cmd_table *cmd)
+{
+	int flags;
+	int outfd;
+
+	flags = O_WRONLY | O_CREAT;
+	if (cmd->append_flag)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	outfd = open(cmd->outfile, flags, 0644);
+	if (outfd < 0)
+		error_and_exit(cmd->outfile, msh);
+	dup2(outfd, STDOUT_FILENO);
+	close(outfd);
+}
+
+static void	child_processs(t_cmd_table *cmd, int in_fd, int *fd,
+		t_minishell *msh)
+{
+	if (cmd->infile)
+		handle_infile(msh, cmd);
+	else if (in_fd != 0)
+	{
+		dup2(in_fd, STDIN_FILENO);
+		close(in_fd);
+	}
+	if (cmd->outfile)
+		handle_outfile(msh, cmd);
+	else if (cmd->next)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+	}
+	if (fd[0])
+		close(fd[0]);
+	msh->cmd_table = cmd;
+	exec_cmd(msh);
+	exit(EXIT_FAILURE);
+}
+
+static void	parent_process(int *in_fd, int *fd, t_cmd_table *cmd)
+{
+	if (*in_fd != 0)
+		close(*in_fd);
+	if (cmd->next)
+	{
+		close(fd[1]);
+		*in_fd = fd[0];
+	}
+}
+
+static int	single_builtin(t_cmd_table *cmd, t_minishell *msh)
+{
+	if (cmd && !cmd->next && !cmd->infile && !cmd->outfile && run_builtin(msh))
+		return (1);
+	return (0);
+}
+
+static int	exec_loop(t_minishell *msh)
+{
+	t_cmd_table	*cmd;
+	int			fd[2];
+	int			in_fd;
+	pid_t		pid;
+	int			status;
+
+	cmd = msh->cmd_table;
+	in_fd = 0;
+	while (cmd)
+	{
+		if (cmd->next && pipe(fd) == -1)
+			return (perror("pipe"), -1);
+		pid = fork();
+		if (pid == -1)
+			return (perror("fork"), -1);
+		if (pid == 0)
+			child_processs(cmd, in_fd, fd, msh);
+		parent_process(&in_fd, fd, cmd);
+		cmd = cmd->next;
+	}
+	while (wait(&status) > 0)
+		;
+	return (0);
+}
+
 int	executor(t_minishell *msh)
 {
-	if (!msh->cmd_table || !msh->cmd_table->cmd || !msh->cmd_table->cmd[0])
-		return (-1);
-	if (run_builtin(msh))
+	if (single_builtin(msh->cmd_table, msh))
 		return (0);
-	return (exec_cmd(msh));
+	return (exec_loop(msh));
 }
--- srcs/main.c
+++ srcs/main.c
@@ -63,6 +63,109 @@
 // 	printf("--- End of Command Table ---\n");
 // }
 
+//static void	test_executor_out_redir(t_minishell *minishell)
+//{
+//    t_cmd_table *cmd1 = calloc(1, sizeof(t_cmd_table));
+
+//    // Command: ls > out.txt
+//    cmd1->cmd = calloc(2, sizeof(char *));
+//    cmd1->cmd[0] = strdup("ls");
+//    cmd1->cmd[1] = NULL;
+//    cmd1->outfile = strdup("out.txt");
+//    cmd1->append_flag = 0; // 0 for >, 1 for >>
+
+//    cmd1->next = NULL;
+
+//    minishell->cmd_table = cmd1;
+
+//    executor(minishell);
+//}
+
+//static void	test_executor_in_redir(t_minishell *minishell)
+//{
+//    t_cmd_table *cmd1 = calloc(1, sizeof(t_cmd_table));
+
+//    // Command: cat < in.txt
+//    cmd1->cmd = calloc(2, sizeof(char *));
+//    cmd1->cmd[0] = strdup("cat");
+//    cmd1->cmd[1] = NULL;
+//    cmd1->infile = strdup("in.txt");
+
+//    cmd1->next = NULL;
+
+//    minishell->cmd_table = cmd1;
+
+//    executor(minishell);
+//}
+
+//static void	test_executor_in_out_redir(t_minishell *minishell)
+//{
+//    t_cmd_table *cmd1 = calloc(1, sizeof(t_cmd_table));
+
+//    // Command: cat < in.txt > out.txt
+//    cmd1->cmd = calloc(2, sizeof(char *));
+//    cmd1->cmd[0] = strdup("cat");
+//    cmd1->cmd[1] = NULL;
+//    cmd1->infile = strdup("in.txt");
+//    cmd1->outfile = strdup("out.txt");
+//    cmd1->append_flag = 0;
+
+//    cmd1->next = NULL;
+
+//    minishell->cmd_table = cmd1;
+
+//    executor(minishell);
+//}
+
+//static void	test_executor_pipe_redir(t_minishell *minishell)
+//{
+//    t_cmd_table *cmd1 = calloc(1, sizeof(t_cmd_table));
+//    t_cmd_table *cmd2 = calloc(1, sizeof(t_cmd_table));
+
+//    // Command 1: cat < in.txt
+//    cmd1->cmd = calloc(2, sizeof(char *));
+//    cmd1->cmd[0] = strdup("cat");
+//    cmd1->cmd[1] = NULL;
+//    cmd1->infile = strdup("in.txt");
+//    cmd1->next = cmd2;
+
+//    // Command 2: grep foo > out.txt
+//    cmd2->cmd = calloc(3, sizeof(char *));
+//    cmd2->cmd[0] = strdup("grep");
+//    cmd2->cmd[1] = strdup("foo");
+//    cmd2->cmd[2] = NULL;
+//    cmd2->outfile = strdup("out.txt");
+//    cmd2->append_flag = 0;
+//    cmd2->next = NULL;
+
+//    minishell->cmd_table = cmd1;
+
+//    executor(minishell);
+//}
+
+//static void	test_executor(t_minishell *minishell)
+//{
+//    t_cmd_table *cmd1 = calloc(1, sizeof(t_cmd_table));
+//    t_cmd_table *cmd2 = calloc(1, sizeof(t_cmd_table));
+
+//    // Command 1: ls
+//    cmd1->cmd = calloc(2, sizeof(char *));
+//    cmd1->cmd[0] = strdup("ls");
+//    cmd1->cmd[1] = NULL;
+//    cmd1->next = cmd2;
+
+//    // Command 2: wc -l
+//    cmd2->cmd = calloc(3, sizeof(char *));
+//    cmd2->cmd[0] = strdup("wc");
+//    cmd2->cmd[1] = strdup("-l");
+//    cmd2->cmd[2] = NULL;
+//    cmd2->next = NULL;
+
+//    minishell->cmd_table = cmd1;
+
+//    executor(minishell);
+//}
+
 static int		has_syntax_error(const char *input)
 {
 	int		i;
@@ -130,8 +233,12 @@ int main(int argc, char *argv[], char *envp[])
 	}
 	//handle_signals();
 	init_minishell(&minishell, envp);
-	set_signal_protocal(&minishell, main_shell);
+	//test_executor_out_redir(&minishell);
+	//test_executor_in_redir(&minishell);
+	//test_executor_in_out_redir(&minishell);
+	//test_executor_pipe_redir(&minishell);
 	run_minishell(&minishell);
+	set_signal_protocal(&minishell, main_shell);
 	rl_clear_history();
 	//exit_minishell();
 }
